5827d621a709fb7246ee56004e348cdf29574a81
Aakash Shah <aakash.shah1@tesco.com>
2024-12-28 17:28:58 +0530

    chore: setup tailwind in mhw-pages

--- pages/mhw-pages/lib/components/Button.tsx
+++ pages/mhw-pages/lib/components/Button.tsx
@@ -1,5 +1,11 @@
 export const Button = (
   props: React.ButtonHTMLAttributes<HTMLButtonElement>
 ) => {
-  return <button {...props} style={{ backgroundColor: "green" }} />;
+  return (
+    <button
+      className="bg-cyan-300"
+      {...props}
+      style={{ backgroundColor: "green" }}
+    />
+  );
 };
