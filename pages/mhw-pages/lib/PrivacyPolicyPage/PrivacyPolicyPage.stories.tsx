76df85ceb9b3e9e6e385846cf7c91f2a4a30f31e
Aakash Shah <aakash.shah1@tesco.com>
2025-01-02 10:14:15 +0530

    chore: setup privacy policy

---
+++ pages/mhw-pages/lib/PrivacyPolicyPage/PrivacyPolicyPage.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { PrivacyPolicyPage } from "./PrivacyPolicyPage";
+
+const meta = {
+  title: "Pages/Privacy Policy Page/Privacy Policy Page",
+  component: PrivacyPolicyPage,
+  parameters: {
+    layout: "fullscreen",
+  },
+  args: {},
+} satisfies Meta<typeof PrivacyPolicyPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const HomePageExample: Story = {};
+HomePageExample.storyName = "Privacy Policy Page";
