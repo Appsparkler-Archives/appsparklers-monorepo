7dd64b8769c9679d305a9fa0072f3372fff35001
Aakash Shah <aakash.shah1@tesco.com>
2025-01-03 16:22:04 +0530

    chore: wip

--- apps/mhw-app/app/routes/privacy.tsx
+++ apps/mhw-app/app/routes/privacy.tsx
@@ -11,6 +11,14 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+/*************  ✨ Codeium Command ⭐  *************/
+/**
+ * PrivacyPage component renders the PrivacyPolicyPage component.
+ * This component is responsible for displaying the privacy policy
+ * information to the user.
+ */
+
+/******  efdd3824-ed5e-4cdc-beac-a9d1c47b1785  *******/
 export default function PrivacyPage() {
   return <PrivacyPolicyPage />;
 }
