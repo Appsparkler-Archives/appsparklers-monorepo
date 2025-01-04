101b01ad3204169642dd537eadc9c5cd6fb5214f
Aakash Shah <aakash.shah1@tesco.com>
2025-01-04 09:13:19 +0530

    chore: wip

--- pages/mhw-pages/lib/HomePage/components/AsyncFileDownloadButton.stories.tsx
+++ pages/mhw-pages/lib/HomePage/components/AsyncFileDownloadButton.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { AsyncFilieDownloadButton } from "./AsyncFilieDownloadButton";
+import { fn } from "@storybook/test";
 
 const meta = {
   title: "Pages/Home Page/Async File Download Button",
@@ -7,11 +8,18 @@ const meta = {
   parameters: {
     layout: "fullscreen",
   },
-  args: {},
+  args: {
+    pdfDownloadLink:
+      "https://firebasestorage.googleapis.com/v0/b/my-hfn.firebasestorage.app/o/messages%2F2025-01-04.pdf?alt=media&token=87317045-471f-4933-a69c-52bbe4de1983",
+    inProgress: false,
+    onClick: fn(),
+  },
 } satisfies Meta<typeof AsyncFilieDownloadButton>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  args: meta.args,
+};
 Primary.storyName = "Async File Download Button";
--- pages/mhw-pages/lib/HomePage/components/AsyncFilieDownloadButton.tsx
+++ pages/mhw-pages/lib/HomePage/components/AsyncFilieDownloadButton.tsx
@@ -1,6 +1,22 @@
-export const AsyncFilieDownloadButton: React.FC = () => {
+import { useCallback } from "react";
+
+export const AsyncFilieDownloadButton = ({
+  pdfDownloadLink,
+}: {
+  onClick: () => Promise<void>;
+  inProgress: boolean;
+  pdfDownloadLink: string;
+}) => {
+  const handleClick = useCallback(async () => {
+    await fetch(pdfDownloadLink);
+  }, [pdfDownloadLink]);
+
   return (
-    <button className="btn btn-primary rounded-none" type="button">
+    <button
+      className="btn btn-primary rounded-none"
+      type="button"
+      onClick={handleClick}
+    >
       Download <span className="loading loading-dots loading-sm"></span>
     </button>
   );
